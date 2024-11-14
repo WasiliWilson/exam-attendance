db7a974cee1660b402242a80dc7eeba7cbd70492
WasiliWilson <bed-com-45-21@unima.ac.mw>
2024-11-14 22:54:49 +0200

    first commit on the branch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CourseSelection from "./screens/CourseSelection";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <CourseSelection />
+    </div>
+  );
+}
+
+export default App;
